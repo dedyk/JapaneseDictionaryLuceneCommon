c57080e3e506fed52763c9e6e5fcbbd364b91feb
Fryderyk Mazurek <fryderyk.mazurek@gmail.com>
2021-04-25 22:12:41 +0200

    Generowanie cache podczas generowania podpowiadacza

--- src/test/java/pl/idedyk/japanese/dictionary/lucene/test/Test2.java
+++ src/test/java/pl/idedyk/japanese/dictionary/lucene/test/Test2.java
@@ -2,21 +2,14 @@ package pl.idedyk.japanese.dictionary.lucene.test;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.util.List;
 
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.search.spell.LuceneDictionary;
 import org.apache.lucene.search.suggest.Lookup;
 import org.apache.lucene.search.suggest.Lookup.LookupResult;
 import org.apache.lucene.search.suggest.analyzing.AnalyzingSuggester;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
 import pl.idedyk.japanese.dictionary.lucene.LuceneAnalyzer;
-import pl.idedyk.japanese.dictionary.lucene.LuceneDatabaseSuggesterAndSpellCheckerSource;
 
 public class Test2 {
 
@@ -46,7 +39,7 @@ public class Test2 {
 		
 		lookup.load(new FileInputStream(new File("/tmp/a/AAAA")));
 		
-		List<LookupResult> lookupResultList = lookup.lookup("ook", false, 30);
+		List<LookupResult> lookupResultList = lookup.lookup("kot", false, 30);
 		
 		for (LookupResult lookupResult : lookupResultList) {
 			System.out.println(lookupResult.key);
